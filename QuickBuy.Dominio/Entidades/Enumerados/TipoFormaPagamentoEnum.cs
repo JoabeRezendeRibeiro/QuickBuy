4bdb2241ce940186c30ed068137573c1389b1ee9
Joabe Rezende Ribeiro <57463461+JoabeRezendeRibeiro@users.noreply.github.com>
2020-05-04 21:06:38 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/Entidades/Enumerados/TipoFormaPagamentoEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades.Enumerados
+{
+    public enum TipoFormaPagamentoEnum
+    {
+        NaoDefinido = 1,
+        Boleto = 2,
+        CartaoDeCredito = 3,
+        Deposito = 3
+    }
+
+}
---
+++ QuickBuy.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string email { get; set; }
+
+        public string senha { get; set; }
+
+        public string SobreNome { get; set; }
+
+        /// <summary>
+        /// Usuario pode ter nenhum ou muitos pedidos 
+        /// </summary>
+        public ICollection<Pedido>Pedidos { get; set; }
+
+
+
+    }
+}
