981e889adb31f4150d70d12bd2a8fd5b51a8de6b
Joabe Rezende Ribeiro <57463461+JoabeRezendeRibeiro@users.noreply.github.com>
2021-02-15 14:28:01 -0300

    Criação do repositorio

---
+++ QuickBuy.Repositorio/Contexto/QuickBuy.Repositorio.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickBuy.Dominio.Entidades;
+using QuickBuy.Dominio.Entidades.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Repositorio.Contexto
+{
+    public class QuickBuy : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DbSet<Pedido> Pedidos { get; set; }
+
+        public DbSet<ItemPedido> ItemPedidos { get; set; }
+
+        public DbSet<FormaPagamento> FormaPagamento { get; set; }
+
+    }
+}
