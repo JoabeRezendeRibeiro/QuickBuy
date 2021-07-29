8d15112f6d0bb27c01d83e90492c580561a3cb67
Joabe Rezende Ribeiro <57463461+JoabeRezendeRibeiro@users.noreply.github.com>
2021-07-29 20:07:10 -0300

    Corrigido redirecionamento da rota

---
+++ QuickBuy.web/ClientApp/src/app/autorizacao/guarda.rotas.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=guarda.rotas.js.map
--- QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,27 +1,35 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../modelo/usuario";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
-export class LoginComponent {
-  public usuario = new Usuario();
+export class LoginComponent implements OnInit{
+  public usuario;// = new Usuario();
+  public returnUrl: string;
+    
+  
 
-    public usuarioAutenticado: boolean;
 
-  constructor() {
-    this.usuario = new Usuario();
+ public usuarioAutenticado: boolean;
+
+  constructor(private router: Router, private ActivatedRouter: ActivatedRoute) { 
   }
-
-
- 
+    ngOnInit(): void {
+      this.returnUrl = this.ActivatedRouter.snapshot.queryParams['returnUrl'];
+      this.usuario = new Usuario();
+    }
 
   entrar() {
     if (this.usuario.email == "joabesk@gmail.com" && this.usuario.senha == "ti!gers38") {
 
-      this.usuarioAutenticado = true;
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate([this.returnUrl]);
+
+     this.usuarioAutenticado = true;
     }
    
   }
